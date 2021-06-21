9c45bcb6786e4da1a26940cb352ac73fae85eec4
abdelrahman436 <Abdulrhmann_Bassunii@yahoo.com>
2021-06-21 11:41:38 +0300

    intial commite

--- main.py
+++ main.py
@@ -1,32 +1,94 @@
-from string import Template
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from typing import List
 
 
-class MyTemplate(Template):
-    delimiter = '#'
+class Context():
+    """
+    The Context defines the interface of interest to clients.
+    """
+
+    def __init__(self, strategy: Strategy) -> None:
+        """
+        Usually, the Context accepts a strategy through the constructor, but
+        also provides a setter to change it at runtime.
+        """
+
+        self._strategy = strategy
+
+    @property
+    def strategy(self) -> Strategy:
+        """
+        The Context maintains a reference to one of the Strategy objects. The
+        Context does not know the concrete class of a strategy. It should work
+        with all strategies via the Strategy interface.
+        """
+
+        return self._strategy
+
+    @strategy.setter
+    def strategy(self, strategy: Strategy) -> None:
+        """
+        Usually, the Context allows replacing a Strategy object at runtime.
+        """
+
+        self._strategy = strategy
+
+    def do_some_business_logic(self) -> None:
+        """
+        The Context delegates some work to the Strategy object instead of
+        implementing multiple versions of the algorithm on its own.
+        """
+
+        # ...
+
+        print("Context: Sorting data using the strategy (not sure how it'll do it)")
+        result = self._strategy.do_algorithm(["a", "b", "c", "d", "e"])
+        print(",".join(result))
+
+        # ...
 
 
-def Main():
-    cart = []
-    cart.append(dict(item="desk", price=12, qty=3))
-    cart.append(dict(item="chair", price=96, qty=2))
-    cart.append(dict(item="notebooks", price=45, qty=2))
-    cart.append(dict(item="pencils", price=24, qty=1))
-    cart.append(dict(item="stapler", price=7, qty=1))
-    cart.append(dict(item="scissors", price=65, qty=1))
-    cart.append(dict(item="compass", price=8, qty=1))
-    cart.append(dict(item="ballpoints", price=65, qty=2))
-    cart.append(dict(item="calculator", price=84, qty=1))
-    cart.append(dict(item="palette", price=1944, qty=2))
+class Strategy(ABC):
+    """
+    The Strategy interface declares operations common to all supported versions
+    of some algorithm.
 
-    t = MyTemplate("#qty x #item = #price")
-    total = 0
-    print("Cart:")
-    for data in cart:
-        print(t.substitute(data))
-        total += data["price"]
-    print("Total: " + str(total))
+    The Context uses this interface to call the algorithm defined by Concrete
+    Strategies.
+    """
+
+    @abstractmethod
+    def do_algorithm(self, data: List):
+        pass
 
 
-if __name__ == '__main__':
-    Main()
+"""
+Concrete Strategies implement the algorithm while following the base Strategy
+interface. The interface makes them interchangeable in the Context.
+"""
 
+
+class ConcreteStrategyA(Strategy):
+    def do_algorithm(self, data: List) -> List:
+        return sorted(data)
+
+
+class ConcreteStrategyB(Strategy):
+    def do_algorithm(self, data: List) -> List:
+        return reversed(sorted(data))
+
+
+if __name__ == "__main__":
+    # The client code picks a concrete strategy and passes it to the context.
+    # The client should be aware of the differences between strategies in order
+    # to make the right choice.
+
+    context = Context(ConcreteStrategyA())
+    print("Client: Strategy is set to normal sorting.")
+    context.do_some_business_logic()
+    print()
+
+    print("Client: Strategy is set to reverse sorting.")
+    context.strategy = ConcreteStrategyB()
+    context.do_some_business_logic()
